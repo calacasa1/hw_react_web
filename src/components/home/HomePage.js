7783b07408068bced590fb0496b6a513ecf7164f
Carlos A Lacasa <chucklacasa@gmail.com>
2022-01-21 14:52:02 -0500

    Even better description

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => (
   <div className="jumbotron">
-    <h1>A site built using React and Redux</h1>
-    <p>A Static Web App hosted on Azure with a CI/CD pipeline into a repo hosted on Github</p>
+    <h1>A site built using React and Redux...</h1>
+    <p>and hosted as a Static Web App on Azure with a CI/CD pipeline into a Github repo!</p>
     <Link to="about" className="btn btn-primary btn-lg">
       Learn more
     </Link>
