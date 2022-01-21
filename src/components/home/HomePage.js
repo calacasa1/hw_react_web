b4c7e6e167487191e1c8d8ac8d1f2b0d90ceedb5
Carlos A Lacasa <chucklacasa@gmail.com>
2022-01-21 14:45:22 -0500

    Changed descriptions

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => (
   <div className="jumbotron">
-    <h1>Pluralsight Administration</h1>
-    <p>React, Redux and React Router for ultra-responsive web apps.</p>
+    <h1>A site built using React and Redux</h1>
+    <p>A Static Web App hosted on Azure with a CI/CD pipeline into a repo hosted on Github</p>
     <Link to="about" className="btn btn-primary btn-lg">
       Learn more
     </Link>
