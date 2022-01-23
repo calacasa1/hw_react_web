fc922f0e49310f355e7da1b503e27198f99414c6
Carlos A Lacasa <chucklacasa@gmail.com>
2022-01-23 10:24:27 -0500

    update message

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const HomePage = () => (
   <div className="jumbotron">
     <h1>Built using React...</h1>
-    <p>and hosted as a Static Web App on Azure with a CI/CD pipeline into a Github repo!</p>
+    <p>and hosted as a Static Web App on Azure with a CI/CD pipeline from a Github repo!</p>
     <Link to="about" className="btn btn-primary btn-lg">
       Learn more
     </Link>
